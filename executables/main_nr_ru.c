8599b9d667642215b9037a4009b9027050ebec6f
Rúben Soares Silva <rsilva@allbesmart.pt>
2026-09-14 15:27:17 +0100

    Include nr_modulation.h in main_nr_ru.c
    Include necessary header to prevent implicit
    declaration warnings on `init_symbol_rotation(fp)`
    and `init_timeshift_rotation`.
    
    Signed-off-by: Rúben Soares Silva <rsilva@allbesmart.pt>

--- executables/main_nr_ru.c
+++ executables/main_nr_ru.c
@@ -23,6 +23,7 @@
 #include <executables/thread-common.h>
 #include "executables/nr-softmodem.h"
 #include "nr-oru.h"
+#include "PHY/MODULATION/nr_modulation.h"
 #include "common/utils/threadPool/thread-pool.h"
 #include "openair1/PHY/INIT/nr_phy_init.h"
 #include "openair1/SCHED_NR/sched_nr.h"
